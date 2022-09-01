d7586b63e6a8df17f1e049bead554986c2e0b892
Sachin Rao <sachinrao9450@gmail.com>
2022-09-01 15:26:42 +0530

    first commit

---
+++ Enough_space/script.js
@@ -0,0 +1,11 @@
+function enough_space(m_size, f_x, f_y){
+    if (f_x+2*f_y<=m_size)
+    {
+        document.write("Ram can be save the file")
+    }
+    else{
+        document.write("Ram can not be save the file \n\becouse the file size is larger then the available space of memory");
+    }
+
+}
+enough_space(6,1,2)
