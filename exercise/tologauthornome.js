d753a491822c195f1064a19e56464cd0b959610c
Sachin Rao <sachinrao9450@gmail.com>
2022-09-07 15:48:40 +0530

    log author name from array of object

---
+++ exercise/tologauthornome.js
@@ -0,0 +1,26 @@
+var library = [ 
+    {
+        author: 'Mark Zukerberg',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+ 
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+ 
+    {
+        author: 'Elon Musk',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    }
+ ];
+ for(let i=0;i<=library.length-1;i++)
+ {
+    let name=library[i].author;
+    console.log(name);
+ }
+ //console.log(library);
+ 
