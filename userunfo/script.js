9d2fd68a2adbb83e6a4d5db22c73ebe716e3b2f6
Sachin Rao <sachinrao9450@gmail.com>
2022-09-09 10:55:54 +0530

    usr infomation

---
+++ userunfo/script.js
@@ -0,0 +1,24 @@
+const users = [
+  { name: "Raj", age: 25 },
+  { name: "Mukesh", age: 33 },
+  { name: "Amita", age: 55 },
+  { name: "Yash", age: 22 },
+];
+
+let count = 1;
+for (let i = 0; i <= users.length - 1; i++) {
+  var x = document.querySelector(".info_txt");
+  const div1 = document.createElement("div");
+  const p1 = document.createElement("p");
+  p1.innerText = `${users[i].name}`;
+  const p2 = document.createElement("p");
+  p2.innerText = `${users[i].age}`;
+  const p3 = document.createElement("p");
+  p3.innerText = `${count}`;
+  div1.appendChild(p3);
+  div1.appendChild(p1);
+  div1.appendChild(p2);
+
+  x.appendChild(div1);
+  count++;
+}
