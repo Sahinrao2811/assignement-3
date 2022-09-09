038b1aad74d198b3ac292098f1fe0818320456c7
Sachin Rao <sachinrao9450@gmail.com>
2022-09-09 15:13:36 +0530

    css change

--- userunfo/script.js
+++ userunfo/script.js
@@ -18,7 +18,7 @@ for (let i = 0; i <= users.length - 1; i++) {
   div1.appendChild(p3);
   div1.appendChild(p1);
   div1.appendChild(p2);
-
   x.appendChild(div1);
+  div1.style = "display: flex ;justify-content :space-between ;align-items: center;margin-right: 50px;"
   count++;
 }
